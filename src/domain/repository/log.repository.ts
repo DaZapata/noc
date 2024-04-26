745cd9c3a3bd80a253bc13f17ec29e3864170d65
David Zapata <david.zapata@controlt.com.co>
2024-04-26 11:42:29 -0500

    implements saveLog

---
+++ src/domain/repository/log.repository.ts
@@ -0,0 +1,7 @@
+import { LogEntity, LogSeverityLevel } from "../entities/log.entity";
+
+// abstract para que no se pueda instanciar esta clase
+export abstract class LogRepository {
+    abstract saveLog(log: LogEntity): Promise<void>;
+    abstract getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity>;
+}
